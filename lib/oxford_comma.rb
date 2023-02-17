87639e1ddacf9f32c07781816a6964703481949d
Isaac Ongera <isaacongera717@gmail.com>
2023-02-17 15:52:36 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+
 def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+    end
 
 end
